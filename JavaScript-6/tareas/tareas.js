c83be7059b46f4189c5cac4541f146e2b1a97498
Axel Mullins <axelmullins94@gmail.com>
2022-02-16 20:16:13 -0300

    Subida a GitHub

---
+++ JavaScript-6/tareas/tareas.js
@@ -0,0 +1,77 @@
+// No cambies los nombres de las funciones
+
+function mayuscula(nombre) {
+  //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
+  //ej: Recibe "mario" ----> Devuelve "Mario"
+  //Tu código:
+  return nombre.charAt(0).toUpperCase() + nombre.slice(1); 
+    // 1ro agarramos la primer letra y la cambiamos a mayúscula, devuielve "M"
+    // 2do agarramos el string `nombre` y eliminamos todos los elementos hatas el indice 1, devuelve "ario"
+    // 3ro concatenamos los string
+}
+
+function invocarCallback(cb) {
+  // Invoca al callback `cb`
+  //Tu código:
+  cb()
+}
+
+function operacionMatematica(n1, n2, cb) {
+  //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
+  //Devolver el callback pasándole como argumentos los números recibidos.
+  //Tu código:
+  cb(n1, n2)
+}
+
+function sumarArray(numeros, cb) {
+  // Suma todos los números enteros (int/integers) de un array ("numeros")
+  // Pasa el resultado a `cb`
+  // No es necesario devolver nada
+  //Tu código:
+  let resultado = 0
+  for (let i = 0; i < numeros.length; i++) {
+    resultado += numeros[i];
+  }
+  cb(resultado)
+}
+
+function forEach(array, cb) {
+  // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
+  // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
+  //Tu código:
+  array.forEach(element => {
+    cb(element);
+  });
+}
+
+function map(array, cb) {
+  // Crea un nuevo array
+  // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
+  // El nuevo array debe tener la misma longitud que el array del argumento
+  //Tu código:
+  const nuevoArray = array.map(function (x) {
+    return cb(x)
+  })
+  return nuevoArray
+}
+
+function filter(array) {
+  //Filtrar todos los elementos del array que comiencen con la letra "a".
+  //Devolver un nuevo array con los elementos que cumplen la condición
+  //Tu código:
+  const nuevoArray = array.filter(palabra => palabra[0] === "a")
+  return nuevoArray
+}
+
+// No modificar nada debajo de esta línea, sino no correrán los test!
+// --------------------------------
+
+module.exports = {
+  mayuscula,
+  invocarCallback,
+  operacionMatematica,
+  sumarArray,
+  forEach,
+  map,
+  filter
+};
---
+++ README.md
@@ -0,0 +1,36 @@
+# 📖 Test JavaScript-2
+
+Desafíos en Academia Numen.
+
+## ¿Para qué sirve?
+
+- Practicar conocimientos adquiridos mediante la resolución de problemas.
+
+## Herramientas 🛠️
+
+- **JavaScript**
+    - Objetos
+    - Clases
+    - Callback
+    - ForEach
+    - Map
+    - Filter
+
+## Ejecutando los test
+
+Desde la carpeta NUMEN-JS-II (Donde se encuentra el archivo package.json) deberán escribir 'npm install' (Sin las comillas) en la terminal para que se instalen las dependencias necesarias para que puedan ejecutarse los testing, caso contrario no se ejecutarán.
+
+Para ejecutar cada uno de los test dividos por niveles es necesario que escriban en la terminal desde la carpeta Numen-JS-II :
+
+* npm test JavaScript-4
+
+* npm test JavaScript-5
+
+* npm test JavaScript-6
+
+Según la tarea que quieran evaluar.
+
+## Notas 📢
+
+- [Axel Mullins](https://github.com/AxelMullins) 💻
+- Encontrame en [LinkedIn](https://www.linkedin.com/in/axel-mullins/)
